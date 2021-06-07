ab4134038c6bafa699b2df38ee9190bc20b52c9e
prometheus800 <hristijankocev@ymail.com>
2021-06-07 18:40:30 +0200

    Removed unused directives

--- MainForm.cs
+++ MainForm.cs
@@ -1,17 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Runtime.Remoting.Channels;
-using System.Security;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Windows.Forms.VisualStyles;
 using Newtonsoft.Json;
 using static YuGiOh.MenuScreenForm;
 
